2679cd73fa601299c7276c1046f3afab4a828b88
gilsaia <794433219@qq.com>
2020-02-01 20:25:52 +0800

    add README.md

--- dist/main.js
+++ dist/main.js
@@ -1,16 +1,19 @@
-var watcher=require('./watch-client');
-var produce=require('./spawn');
-var rolehandler=require('./rolehandler');
-module.exports.loop = function(){
-    for(const spawn_name in Game.spawns){
+var watcher = require('./watch-client');
+var produce = require('./spawn');
+var rolehandler = require('./rolehandler');
+module.exports.loop = function () {
+    for (const spawn_name in Game.spawns) {
         produce.run(Game.spawns[spawn_name]);
     }
-    for(const creep_name in Memory.creeps){
-        if(Game.creeps[creep_name] == undefined){
+    for (const room_pos in Game.rooms) {
+
+    }
+    for (const creep_name in Memory.creeps) {
+        if (Game.creeps[creep_name] == undefined) {
             delete Memory.creeps[creep_name];
             continue;
         }
-        var creep=Game.creeps[creep_name];
+        var creep = Game.creeps[creep_name];
         rolehandler.run(creep);
     }
     watcher();
