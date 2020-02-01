994edf02faeae436e934eca0feeb41e823d7a753
gilsaia <794433219@qq.com>
2020-02-01 20:43:38 +0800

    add tower logic

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,18 @@
 var watcher = require('./watch-client');
-var produce = require('./spawn');
+var spawn = require('./spawn');
+var tower = require('./tower');
 var rolehandler = require('./rolehandler');
 module.exports.loop = function () {
     for (const spawn_name in Game.spawns) {
-        produce.run(Game.spawns[spawn_name]);
+        spawn.run(Game.spawns[spawn_name]);
     }
-    for (const room_pos in Game.rooms) {
-
+    for (const room_pos of Game.rooms) {
+        var towers = room_pos.find(FIND_MY_STRUCTURES, {
+            filter: (s) => s.structureType == STRUCTURE_TOWER
+        });
+        for (let m_tower of towers) {
+            tower.defense(m_tower);
+        }
     }
     for (const creep_name in Memory.creeps) {
         if (Game.creeps[creep_name] == undefined) {
