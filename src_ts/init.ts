c26c6d460ebc87cabb0ef9b6dbce613c43fd032e
gilsaia <794433219@qq.com>
2022-01-25 00:49:23 +0800

    delete

--- src_ts/init.ts
+++
@@ -1,7 +0,0 @@
-import { pollingSetup } from './module/pollingQueue';
-import { mountInit } from './mount';
-
-export function init(): void {
-  mountInit();
-  pollingSetup();
-}
--- src_ts/module/autoPlan/index.ts
+++
@@ -1,9 +0,0 @@
-import { BaseAutoPlan } from './base';
-import container from './container';
-import { GetCorePos } from './core';
-
-export const autoPlanController: AutoPlanController = {
-  autoPlanContainer: container,
-  autoPlanCore: GetCorePos,
-  autoPlanBase: BaseAutoPlan
-};
