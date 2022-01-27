2b6fdf187249a75b8db43248cb800cc76204e7c0
gilsaia <794433219@qq.com>
2022-01-28 00:52:11 +0800

    feat: queue test

--- src/infrastructure/circle_queue/circle_queue.test.ts
+++ src/infrastructure/circle_queue/circle_queue.test.ts
@@ -1,7 +1,38 @@
 import { circleQueue } from './index';
+import { random } from 'lodash';
 
-it('可以正常初始化', () => {
+it('初始化', () => {
+  const queue: circleQueue<number> = new circleQueue<number>(10);
+  expect(queue).toBeDefined();
+});
+
+it('获取/设置第k个', () => {
+  const queue: circleQueue<number> = new circleQueue<number>(10);
+  queue.set(20);
+  expect(queue.getNextK(0)).toEqual(20);
+  queue.setNextK(23, 45);
+  expect(queue.getNextK(23)).toEqual(45);
+});
+
+it('获取/完成机制', () => {
   const queue: circleQueue<number> = new circleQueue<number>(10);
   queue.set(10);
-  expect(queue.check()).toBe(10);
+  expect(queue.check()).toEqual(10);
+  queue.setNextK(1, 1039);
+  queue.finish();
+  expect(queue.check()).toEqual(1039);
+});
+
+it('循环测试', () => {
+  const queue: circleQueue<number> = new circleQueue<number>(13);
+  const items = [];
+  for (let i = 0; i < 50; ++i) {
+    items.push(random());
+  }
+  for (let i = 0; i < 50; ++i) {
+    expect(queue.check()).not.toEqual(items[i]);
+    queue.set(items[i]);
+    expect(queue.check()).toEqual(items[i]);
+    queue.finish();
+  }
 });
--- src/infrastructure/circle_queue/index.ts
+++ src/infrastructure/circle_queue/index.ts
@@ -1,3 +1,4 @@
+// 循环队列基础实现 实现特定方法提高效率
 export class circleQueue<T> {
   private readonly items: T[];
   private index: number;
@@ -7,21 +8,26 @@ export class circleQueue<T> {
     this.index = 0;
     this.limit = limit;
   }
+  // 获取队列当前位置向后的第k个元素
   public getNextK(k: number): T {
     return this.items[(this.index + k) % this.limit];
   }
+  // 设置队列当前位置向后的第k个元素
   public setNextK(k: number, item: T): void {
     const cur = (this.index + k) % this.limit;
     delete this.items[cur];
     this.items[cur] = item;
   }
+  // 设置队列当前位置的元素
   public set(item: T): void {
     delete this.items[this.index];
     this.items[this.index] = item;
   }
+  // 获取队列当前位置的元素
   public check(): T {
     return this.items[this.index];
   }
+  // 当前位置处理完成 向后移动一个位置同时删除当前位置元素
   public finish(): void {
     const cur = this.index;
     this.index = (this.index + 1) % this.limit;
